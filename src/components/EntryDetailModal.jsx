6cba050de018f4c1c0908451f821b1c81ab8ed6b
Daiana <daianachichotky@gmail.com>
2026-01-31 13:29:56 +0100

    changing modal size

--- src/components/EntryDetailModal.jsx
+++ src/components/EntryDetailModal.jsx
@@ -3,20 +3,21 @@ const EntryDetailModal = ({ entry, onClose }) => {
 
   return (
     <dialog open className='modal modal-middle'>
-      <div className='cursor-pointer p-4 bg-green-100 shadow-lg rounded-xl transform hover:scale-95 hover:rotate-1 transition-all duration-300'>
+      <div className='w-120 h-125 cursor-pointer p-4 bg-green-100 shadow-lg rounded-xl transform hover:scale-95 hover:rotate-1 transition-all duration-300'>
+        {/* Image */}
         <img
           src={entry.image}
           alt={entry.title}
           className='rounded-lg mb-4 max-h-64 w-full object-cover'
         />
-
+        {/* Title */}
         <h2
           style={{ fontFamily: "'Patrick Hand', cursive" }}
           className='text-2xl font-bold text-center bg-white'
         >
           {entry.title}
         </h2>
-
+        {/* Date */}
         <p className='text-center text-sm text-gray-500 mb-4 bg-white'>
           {new Date(entry.date).toLocaleDateString('en-EN', {
             day: 'numeric',
@@ -24,14 +25,14 @@ const EntryDetailModal = ({ entry, onClose }) => {
             year: 'numeric',
           })}
         </p>
-
+        {/* Description */}
         <p
           style={{ fontFamily: "'Patrick Hand', cursive" }}
           className='text-gray-700 text-center'
         >
           {entry.content}
         </p>
-
+        {/* Button to close */}
         <div className='modal-action'>
           <button className='btn' onClick={onClose}>
             Close
