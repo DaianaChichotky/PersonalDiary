f2c820eb81e33d39eddfdaf31a7921ee447adb82
Daiana <daianachichotky@gmail.com>
2026-01-31 09:32:19 +0100

    updating header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,6 @@
 const Header = ({ onAddEntry }) => {
   return (
     <header className='navbar bg-neutral text-neutral-content px-6'>
-      <h1 className='text-xl font-semibold'>Personal Diary</h1>
-
       <button className='btn btn-primary ml-auto' onClick={onAddEntry}>
         + Add Entry
       </button>
